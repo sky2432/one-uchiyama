0aa05a5797fbea2d4e199fca18e7b9d0ba17430c
sky2432 <skyarts0122@gmail.com>
2022-06-04 17:47:17 +0900

    ONE-10 add Episode and Word model

--- one/admin.py
+++ one/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Episode, Word
 
-# Register your models here.
+admin.site.register(Episode)
+admin.site.register(Word)
--- one/models.py
+++ one/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Episode(models.Model):
+    id = models.PositiveBigIntegerField(primary_key=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Word(models.Model):
+    episode_id = models.ForeignKey(Episode, on_delete=models.CASCADE)
+    surface = models.CharField(max_length=255)
+    pronunciation = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
