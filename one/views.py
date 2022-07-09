864e7e8ae7cad0d7f5d2b4555a50892a1a2f775b
sky2432 <skyarts0122@gmail.com>
2022-07-09 09:01:31 +0900

    ONE-29 add header

--- one/views.py
+++ one/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.db import transaction
 from django.db.models import Q
-from .models import Radio, Episode, Word
-from .service.aws import transcribe_file, getS3PathFromUrl, get_transcript
-from .service.util import parse
+from .models import Episode
 
 
 def top(request):
@@ -15,7 +12,7 @@ def search(request):
         keyword = request.POST['keyword']
         episodes = Episode.objects.filter(
             Q(word__original_form__contains=keyword) | Q(word__pronunciation__contains=keyword)).distinct()
-        return render(request, 'search.html', {'episodes': episodes})
+        return render(request, 'search.html', {'episodes': episodes, 'keyword': keyword})
     else:
         return redirect('top')
 
