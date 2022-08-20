3c2c9647980a8ee8b6b65189bcb1d18344169131
sky2432 <skyarts0122@gmail.com>
2022-08-20 12:51:11 +0900

    ONE-44 set word start time

---
+++ static/js/search.js
@@ -0,0 +1,14 @@
+$(function () {
+  /**
+   * 単語バッジを押したとき、Spotifyプレイヤーに開始時間を設定する
+   */
+  $('.word-badge').click(function () {
+    const startTime = $(this).find('.start-time').text();
+    const $spotifyPlayer = $(this).parent().prev().find('.spotify-player');
+    const src = $spotifyPlayer.attr('src');
+    const keyword = '&t=';
+    const baseUrl = src.substring(0, src.indexOf(keyword));
+    const urlWithStartTime = baseUrl + keyword + startTime;
+    $spotifyPlayer.attr('src', urlWithStartTime)
+  })
+})
