c2ee6b6df4b64355f8c220088f3f5034833e656f
sky2432 <skyarts0122@gmail.com>
2022-11-12 14:13:51 +0900

    ONE-54 fix environ path

--- one_uchiyama/settings.py
+++ one_uchiyama/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # 環境変数の設定
 
 env = environ.Env()
-env.read_env('.env')
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
